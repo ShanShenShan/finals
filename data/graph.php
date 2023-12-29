80ce2cd0ab7ce6e5fa1f7c3c13d19bf244203dd6
ShanShenShan <shankhylep@gmail.com>
2023-12-29 23:52:19 +0800

    bar graph

--- data/graph.php
+++ data/graph.php
@@ -33,6 +33,68 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $salesData = $search->fetchAll(PDO::FETCH_ASSOC);
 }
+
+$selectedYear = isset($_GET['year']) ? $_GET['year'] : date('Y');
+
+try {
+    // Setting up a connection into the database using constants from connection.php
+    $connection = new PDO("mysql:host=" . host . ";dbname=" . database, user, password);
+    
+    // Generating an error message
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    // Initialize arrays for all months with zero sales for both selected year and the previous year
+    $monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    $quantitiesSelectedYear = array_fill(0, 12, 0); // Assuming 0-indexed months
+    $quantitiesPreviousYear = array_fill(0, 12, 0); // Assuming 0-indexed months
+    
+    // Fetch data from the database for the selected year
+    $sqlSelectedYear = "SELECT MONTH(tr_date) as month, SUM(quantity) as total_quantity
+                        FROM transaction_records tr
+                        JOIN transaction_products tp ON tr.id = tp.tr_id
+                        WHERE YEAR(tr_date) = :selectedYear
+                        GROUP BY MONTH(tr_date)";
+    
+    $stmtSelectedYear = $connection->prepare($sqlSelectedYear);
+    $stmtSelectedYear->bindParam(':selectedYear', $selectedYear, PDO::PARAM_STR);
+    $stmtSelectedYear->execute();
+    
+    // Fill in the actual sales data for the selected year
+    while ($row = $stmtSelectedYear->fetch(PDO::FETCH_ASSOC)) {
+        $month = $row['month'] - 1; // Adjust for 0-indexed array
+        $quantitiesSelectedYear[$month] = $row['total_quantity'];
+    }
+
+    // Fetch data from the database for the previous year
+    $previousYear = $selectedYear - 1;
+    $sqlPreviousYear = "SELECT MONTH(tr_date) as month, SUM(quantity) as total_quantity
+                        FROM transaction_records tr
+                        JOIN transaction_products tp ON tr.id = tp.tr_id
+                        WHERE YEAR(tr_date) = :previousYear
+                        GROUP BY MONTH(tr_date)";
+    
+    $stmtPreviousYear = $connection->prepare($sqlPreviousYear);
+    $stmtPreviousYear->bindParam(':previousYear', $previousYear, PDO::PARAM_STR);
+    $stmtPreviousYear->execute();
+    
+    // Fill in the actual sales data for the previous year
+    while ($row = $stmtPreviousYear->fetch(PDO::FETCH_ASSOC)) {
+        $month = $row['month'] - 1; // Adjust for 0-indexed array
+        $quantitiesPreviousYear[$month] = $row['total_quantity'];
+    }
+
+    // Find the maximum quantity for both years
+    $maxQuantitySelectedYear = max($quantitiesSelectedYear);
+    $maxQuantityPreviousYear = max($quantitiesPreviousYear);
+    
+    // Calculate the upper limit for the Y-axis (rounding off to the nearest tens) for both years
+    $upperLimitSelectedYear = ceil($maxQuantitySelectedYear / 10) * 10;
+    $upperLimitPreviousYear = ceil($maxQuantityPreviousYear / 10) * 10;
+    
+} catch (PDOException $error) {
+    // Display the error message
+    die("Connection has been failed miserably. Reason: " . $error->getMessage());
+}
 ?>
 
 
@@ -49,7 +111,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class="card">
 
                     <div class="card-body">
-                        <div name="graph-tQuantity" style="width: 50%; position: relative;">
+                        <div name="donut-tQuantity" style="width: 50%; position: relative;">
                             <?php if (count($salesData) > 0) : ?>
                                 <canvas id="donutChartContainer" style="height: 300px;"></canvas>
                             <?php else : ?>
@@ -57,17 +119,42 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             <?php endif; ?>
                         </div>
 
-                        <div name="tQuantityFilter" style="width: 50%; position: relative;">
-                        <!-- Date Filter Form -->
-                        <form method="POST" action="">
-                            <label for="start_date">Start:</label>
-                                <input type="date" name="start_date" id="start_date" value="<?php echo isset($startDate) ? $startDate : ''; ?>">
-                            <br><label for="end_date">End:</label>
-                                <input type="date" name="end_date" id="end_date" value="<?php echo isset($endDate) ? $endDate : ''; ?>">
-                                <button type="submit">Filter</button>
-                                <button type="button" onclick="resetDates()">Reset</button>
-                        </form>
+                        <div name="donutQuantityFilter" style="width: 50%; position: relative;">
+                            <!-- Date Filter Form -->
+                            <form method="POST" action="">
+                                <label for="start_date">Start:</label>
+                                    <input type="date" name="start_date" id="start_date" value="<?php echo isset($startDate) ? $startDate : ''; ?>">
+                                <br><label for="end_date">End:</label>
+                                    <input type="date" name="end_date" id="end_date" value="<?php echo isset($endDate) ? $endDate : ''; ?>">
+                                    <button type="submit">Filter</button>
+                                    <button type="button" onclick="resetDates()">Reset</button>
+                            </form>
                         </div>
+
+                        <div name="bar-tQuantity" style="width: 90%; position: relative;">
+                            <?php if (count($salesData) > 0) : ?>
+                                <canvas id="salesChart" width="800" height="400"></canvas>
+                            <?php else : ?>
+                                <p>No data found</p>
+                            <?php endif; ?>
+                        </div>
+
+                        <div name="barQuantityFilter" style="width: 50%; position: relative;">
+                            <!-- Year selector -->
+                            <form method="get">
+                                <label for="year">Select Year:</label>
+                                <select id="year" name="year" onchange="this.form.submit()">
+                                    <?php
+                                    // Generate options for the last few years
+                                    $currentYear = date('Y');
+                                    for ($i = $currentYear; $i >= $currentYear - 10; $i--) {
+                                        echo "<option value=\"$i\" " . ($selectedYear == $i ? "selected" : "") . ">$i</option>";
+                                    }
+                                    ?>
+                                </select>
+                            </form>
+                        </div>
+
                     </div>
                 </div>
             </div>
@@ -114,6 +201,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 });
             <?php endif; ?>
         });
+
+        // Use Chart.js to create a bar graph
+var ctx = document.getElementById('salesChart').getContext('2d');
+var salesChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: <?php echo json_encode($monthNames); ?>,
+        datasets: [{
+            label: 'Total Quantity Sold (' + <?php echo $selectedYear; ?> + ')',
+            data: <?php echo json_encode($quantitiesSelectedYear); ?>,
+            backgroundColor: 'rgba(75, 192, 192, 0.2)',
+            borderColor: 'rgba(75, 192, 192, 1)',
+            borderWidth: 1
+        }, {
+            label: 'Total Quantity Sold (' + <?php echo $previousYear; ?> + ')',
+            data: <?php echo json_encode($quantitiesPreviousYear); ?>,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)', // Different color for the previous year
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+                max: <?php echo max($upperLimitSelectedYear, $upperLimitPreviousYear); ?> // Set the max value for the Y-axis
+            }
+        }
+    }
+});
     </script>
 </body>
 
