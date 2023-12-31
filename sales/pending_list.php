a95a3a06e5cfa1add3d69f8074886937ab7a261b
ShanShenShan <shankhylep@gmail.com>
2023-12-31 19:36:52 +0800

    pending list proper function

--- sales/pending_list.php
+++ sales/pending_list.php
@@ -4,51 +4,27 @@
 <?php require "../includes/sidebar.php"; ?> <!-- Strictly requiring to include the sidebar.php-->
 
 <?php
+    
+    $search = $connection->query("SELECT o_id, u.name as customer_name, po.product_id, po.o_quantity, i.product_name, i.category_id, i.quantity, i.product_points, i.price
+    FROM pending_order_kiosk po
+    JOIN users u ON po.customer_id = u.id
+    JOIN inventory i ON po.product_id = i.id
+    GROUP BY po.o_id");
+    $search->execute();
 
-//$search = $connection->query("SELECT o_id, u.name as customer_name, po.product_id, po.o_quantity, i.product_name, i.category_id, i.quantity, i.product_points, i.price
-//FROM pending_orders po
-//JOIN users u ON po.customer_id = u.unique_id
-// JOIN inventory i ON po.product_id = i.id
-//GROUP BY po.o_id");
-//$search->execute();
+    $orderList = $search->fetchAll(PDO::FETCH_OBJ);
 
-//$orderList = $search->fetchAll(PDO::FETCH_OBJ);
-
-//Selecting all data on the kiosk pending table
-$retrieving_data = $connection->query("SELECT * FROM pending_order_kiosk");
-$retrieving_data->execute();
-$kiosk_data = $retrieving_data->fetchAll(PDO::FETCH_OBJ);
-foreach ($kiosk_data as $data) {
-    $order_id = $data->o_id;
-    $customer_id = $data->customer_id;
-    $product_id = $data->product_id;
-}
-
-// Selecting all the values that should be seen on the modal
-$query = $connection->prepare("SELECT i.product_name, c.category_name, po.o_quantity, i.product_points, i.price,i.quantity as storage
-        FROM pending_order_kiosk po
-        JOIN inventory i ON po.product_id = i.id
-        JOIN category c ON i.category_id = c.id
-        WHERE po.o_id = :orderId");
-$query->bindParam(':orderId', $order_id, PDO::PARAM_INT);
-$query->execute();
-$data = $query->fetchAll(PDO::FETCH_OBJ);
-foreach ($data as $information) {
-    $product_name = $information->product_name;
-    $category_name = $information->category_name;
-    $order_quantity = $information->o_quantity;
-    $totalPrice = $information->price * $information->o_quantity;
-    $storage = $information->storage;
-}
 ?>
 
 <body>
 
     <div class="main-wrapper">
+
+
         <div class="page-wrapper">
-            <div class="content">
-                <div class="page-header">
-                    <div class="page-title">
+    <div class="content">
+        <div class="page-header">
+        <div class="page-title">
                         <h4>Order List</h4>
                         <h6>Manage your Orders</h6>
                     </div>
@@ -61,68 +37,108 @@ foreach ($data as $information) {
                                 <thead>
                                     <tr>
                                         <th>Order ID</th>
+                                        <th>Customer Name</th>
                                         <th>Order Details</th>
                                     </tr>
                                 </thead>
-                                <?php foreach ($kiosk_data as $order) : ?>
+                                <?php foreach($orderList as $order) : ?>
                                     <tbody>
                                         <tr>
-                                            <td><?php echo $order->o_id; ?></td>
+                                            <td><?php echo $order->o_id ;?></td>
+                                            <td><?php echo $order->customer_name;?></td>
                                             <td>
-                                                <a class="me-3" data-bs-toggle="modal" data-bs-target="#reviewModal">
-                                                    <img src="<?php echo FILEPATH; ?>/assets/img/icons/excel.svg" alt="img">
-                                                </a>
+                                            <a class="me-3" onclick="openReviewModal('<?php echo $order->o_id; ?>')">
+                                                <img src="<?php echo FILEPATH;?>/assets/img/icons/excel.svg" alt="img">
+                                            </a>
                                             </td>
                                         </tr>
                                     </tbody>
                                 <?php endforeach; ?>
-                            </table>
-                        </div>
-                    </div>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
-    <!-- Review Modal Content -->
-    <div class="modal fade" id="reviewModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg"> <!-- modal-lg for a larger modal, adjust as needed -->
-            <div class="modal-content">
-                <form action="<?php echo FILEPATH; ?>/process/pos_crud/pending_order.php" method="POST">
-                <?php $_SESSION['orderId'] = $customer_id;?>
-                    <div class="modal-header">
-                        <h5 class="modal-title">Review Order Details</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Product Name</th>
-                                    <th>Category</th>
-                                    <th>Quantity</th>
-                                    <th>Price</th>
-                                </tr>
-                            </thead>
-                            <tbody id="orderDetailsTableBody">
-                                <td value="<?php echo $product_name; ?>"><?php echo $product_name; ?></td>
-                                <td value="<?php echo $category_name; ?>"><?php echo $category_name; ?></td>
-                                <td name="current_quantity" value="<?php echo $order_quantity; ?>"><?php echo $order_quantity; ?></td>
-                                <td value="<?php echo $totalPrice; ?>">₱<?php echo $totalPrice; ?></td>
-                            </tbody>
-                        </table>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                        <input type="submit" class="btn btn-primary" value="Validate" name="pending_list">
-                    </div>
-                </form>
+<!-- Review Modal Content -->
+<div class="modal fade" id="reviewModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg"> <!-- modal-lg for a larger modal, adjust as needed -->
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Review Order Details</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Product Name</th>
+                            <th>Category</th>
+                            <th>Quantity</th>
+                            <th>Total Points</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody id="orderDetailsTableBody">
+                        <!-- Populate this tbody dynamically using AJAX -->
+                    </tbody>
+                </table>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-primary" onclick="validateOrder()">Validate</button>
             </div>
         </div>
     </div>
+</div>
+
+<script>
+    function openReviewModal(orderId) {
+        // Implement AJAX call to fetch order details based on orderId
+        $.ajax({
+            url: '../process/pos_crud/get_order_details.php',
+            method: 'POST',
+            data: {orderId: orderId},
+            success: function(response) {
+                // Update the modal content with the retrieved order details
+                $('#orderDetailsTableBody').html(response);
+
+                // Store the orderId in a data attribute for later use
+                $('#reviewModal').data('orderId', orderId);
+
+                // Show the modal
+                $('#reviewModal').modal('show');
+            },
+            error: function(error) {
+                console.error('Error fetching order details:', error);
+            }
+        });
+    }
+
+    function validateOrder() {
+        // Get the orderId from the data attribute
+        var orderId = $('#reviewModal').data('orderId');
+
+        // Implement AJAX call to transfer entries to pending_orders
+        $.ajax({
+            url: '../process/pos_crud/validate_order.php',
+            method: 'POST',
+            data: {orderId: orderId},
+            success: function(response) {
+                // Redirect to pos.php upon successful validation
+                window.location.href = 'pos.php';
+            },
+            error: function(error) {
+                console.error('Error transferring entries to pending_orders:', error);
+            }
+        });
+    }
+</script>
 
 
-    <?php require "../includes/footer.php"; ?> <!-- Strictly requiring to include the footer.php-->
+
+<?php require "../includes/footer.php"; ?> <!-- Strictly requiring to include the footer.php-->
 </body>
 
 </html>
