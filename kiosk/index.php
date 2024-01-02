71db7281e2107cd1912ae6deefbb931a977589e2
ShanShenShan <shankhylep@gmail.com>
2024-01-03 02:30:20 +0800

    SIDEBAR FIX! CATEGORY FIX!

--- kiosk/index.php
+++ kiosk/index.php
@@ -1,7 +1,7 @@
 <?php require "../config/connection.php"; ?>
 <?php
 session_start();
-define("FILEPATH", "http://localhost/pos1");
+define("FILEPATH", "http://localhost/pos2/finals");
 
 // Fetch the latest o_id from the pending_order_kiosk table
 $latestOrderIdQuery = $connection->query("SELECT MAX(o_id) AS latest_o_id FROM pending_order_kiosk");
@@ -9,27 +9,13 @@ $latestOrderIdResult = $latestOrderIdQuery->fetch(PDO::FETCH_ASSOC);
 $latestOrderId = ($latestOrderIdResult['latest_o_id']) ? $latestOrderIdResult['latest_o_id'] : 0;
 
 
-if(isset($_GET['gp']))
-{
-    $gp = $_GET['gp'];
-    // Displaying all of the items on the webpage
-$select_all = $connection->query("SELECT inventory.*, category.category_name
-FROM inventory
-JOIN category ON inventory.category_id = category.id
-where category.category_name = '$gp'
-");
-$select_all->execute();
-$all_products = $select_all->fetchAll(PDO::FETCH_OBJ);
-}
-else{
-// Displaying all of the items on the webpage
-$select_all = $connection->query("SELECT inventory.*, category.category_name
-FROM inventory
-JOIN category ON inventory.category_id = category.id
-");
-$select_all->execute();
-$all_products = $select_all->fetchAll(PDO::FETCH_OBJ);
-}
+    // Displaying all of the items on the webpage ordered by category_id
+    $select_all = $connection->query("SELECT inventory.*, category.category_name
+                                      FROM inventory
+                                      JOIN category ON inventory.category_id = category.id
+                                      ORDER BY inventory.category_id");
+    $select_all->execute();
+    $all_products = $select_all->fetchAll(PDO::FETCH_OBJ);
 
 
 if (empty($_SESSION['email'])) {
@@ -37,7 +23,7 @@ if (empty($_SESSION['email'])) {
     $default_account = $connection->prepare("SELECT * FROM customers WHERE id = 3007");
     $default_account->execute();
     $default_account_data = $default_account->fetchAll(PDO::FETCH_OBJ);
-    
+
     foreach ($default_account_data as $information) {
         $customer_id = $information->id;
         $order_id = $information->unique_code;
@@ -91,98 +77,98 @@ if (empty($_SESSION['email'])) {
                 <ul class="menu-links">
 
                     <li class="nav-link">
-                        <a href="index.php?gp=Coffee">
+                        <a href="#2">
                             <i class='bx bxs-coffee-bean icon'></i>
                             <span class="text nav-text">Coffee</span>
                         </a>
                     </li>
 
                     <li class="nav-link">
-                        <a href="index.php?gp=Hot Drinks">
+                        <a href="#15">
                             <i class='bx bx-coffee-togo icon'></i>
                             <span class="text nav-text">Hot Drinks</span>
                         </a>
                     </li>
 
                     <li class="nav-link">
-                        <a href="index.php?gp=Frappes">
+                        <a href="#4">
                             <i class='bx bxs-coffee-alt icon'></i>
                             <span class="text nav-text">Frappes</span>
                         </a>
                     </li>
 
                     <li class="nav-link">
-                        <a href="index.php?gp=Iced Drinks">
+                        <a href="#14">
                             <i class='bx bxs-coffee icon'></i>
                             <span class="text nav-text">Iced Drinks</span>
                         </a>
                     </li>
 
                     <li class="nav-link">
-                        <a href="index.php?gp=Milk Teas">
+                        <a href="#6">
                             <i class='bx bxs-leaf icon logout'></i>
                             <span class="text nav-text">Milk Teas</span>
                         </a>
                     </li>
 
                     <li class="nav-link">
-                        <a href="index.php?gp=Fruit Teas">
+                        <a href="#5">
                             <i class='bx bxs-lemon icon'></i>
                             <span class="text nav-text">Fruit Teas</span>
                         </a>
                     </li>
 
                     <li class="nav-link">
-                        <a href="index.php?gp=Softdrinks">
+                        <a href="#1">
                             <i class="fa-solid fa-bottle-water icon"></i>
                             <span class="text nav-text">Softdrinks</span>
                         </a>
                     </li>
 
                     <li class="nav-link">
-                        <a href="index.php?gp=Rice Mealss">
+                        <a href="#13">
                             <i class='bx bxs-bowl-rice icon'></i>
                             <span class="text nav-text">Rice Meals</span>
                         </a>
                     </li>
 
                     <li class="nav-link">
-                        <a href="index.php?gp=All - Day Breakfast">
+                        <a href="#8">
                             <i class="fa-solid fa-egg icon"></i>
                             <span class="text nav-text">All Day Breakfast</span>
                         </a>
                     </li>
 
                     <li class="nav-link">
-                        <a href="index.php?gp=Pasta & Noodles">
+                        <a href="#7">
                             <i class='bx bxs-bowl-hot icon'></i>
                             <span class="text nav-text">Pasta & Noodles</span>
                         </a>
                     </li>
 
                     <li class="nav-link">
-                        <a href="index.php?gp=Kiddie Pasta">
+                        <a href="#11">
                             <i class='bx bxs-bowl-hot icon'></i>
                             <span class="text nav-text">Kiddie Pasta</span>
                         </a>
                     </li>
 
                     <li class="nav-link">
-                        <a href="index.php?gp=Soup & Vegetables">
+                        <a href="#12">
                             <i class='bx bxs-bowl-rice icon'></i>
                             <span class="text nav-text">Soup & Vegetables</span>
                         </a>
                     </li>
 
                     <li class="nav-link">
-                        <a href="index.php?gp=All - Time Favorites">
+                        <a href="#9">
                             <i class='bx bxs-star icon'></i>
                             <span class="text nav-text">All Time Favorites</span>
                         </a>
                     </li>
 
                     <li class="nav-link">
-                        <a href="index.php?gp=Add - Ons">
+                        <a href="#10">
                             <i class='bx bx-cart-add icon'></i>
                             <span class="text nav-text">Add - Ons</span>
                         </a>
@@ -220,31 +206,55 @@ if (empty($_SESSION['email'])) {
 
         </header>
 
-        <div class="listProduct">
-            <?php foreach ($all_products as $product) : ?>
-                <?php
-                    // 
-                    $storage_quantity=$connection->query("SELECT quantity FROM inventory where id =$product->id ");
-                    $storage_quantity->execute();
-                    $quantity=$storage_quantity->fetchColumn();
-                    ?>
-                
-                    <div class="item">
-                        <img src="<?php echo FILEPATH; ?>/assets/img/product/<?php echo $product->image; ?>" alt="" data-productname="<?php echo htmlspecialchars($product->product_name); ?>" data-category="<?php echo htmlspecialchars($product->category_name); ?>" data-price="<?php echo htmlspecialchars($product->price); ?>" data-description="<?php echo htmlspecialchars($product->description); ?>" data-image="<?php echo FILEPATH; ?>/assets/img/product/<?php echo $product->image; ?>" data-id="<?php echo htmlspecialchars($product->id); ?>"> <!-- Include product ID -->
-                        <h5><?php echo htmlspecialchars($product->product_name); ?></h5>
-                        <div class="price">₱<?php echo $product->price; ?></div>
-                        <input type="hidden" value="<?php echo $product->id ?>"><!-- Include product ID -->
-                        <input type="hidden" value="<?php echo $customer_id; ?>">
-                        <input type="hidden" value="<?php echo $quantity; ?>">      <!-- ITO YUNG STORAGE QUANTITY NA GALING SA DATABASE PER PRODUCT-->
-                        <!-- NEED LANG MINUS YUNG BILANG NG BINILI NIYA SA VALUE NITO. BAGO IALAGAY SA DATABASE -->
-                        <button type="submit" name="add-to-cart" class="addCart"> Add To Cart</button>
-                    </div>
-                
-            <?php endforeach; ?>
-        </div>
+        <?php
+        // Initialize variable to track current category
+        $currentCategory = null;
+        $currentCategoryId = null;
+        ?>
 
+        <!-- Displaying products with category as headers -->
+        <?php foreach ($all_products as $product) : ?>
+            <?php
+            // Check if the category has changed
+            if ($currentCategory !== $product->category_name) {
+                // If yes, update the current category and display category header
+                if ($currentCategory !== null) {
+                    // Close the listProduct div if it was open
+                    echo '</div>';
+                }
+                $currentCategory = $product->category_name;
+                $currentCategoryId = $product->category_id;
+                echo "<h2 id='$currentCategoryId'>{$currentCategory}</h2>";
+                echo '<div class="listProduct">';
+            }
+            ?>
+
+            <?php
+            // Retrieve the storage quantity for the current product
+            $storage_quantity = $connection->query("SELECT quantity FROM inventory WHERE id = $product->id");
+            $storage_quantity->execute();
+            $quantity = $storage_quantity->fetchColumn();
+            ?>
+
+            <div class="item">
+                <img src="<?php echo FILEPATH; ?>/assets/img/product/<?php echo $product->image; ?>" alt="" data-productname="<?php echo htmlspecialchars($product->product_name); ?>" data-category="<?php echo htmlspecialchars($product->category_name); ?>" data-price="<?php echo htmlspecialchars($product->price); ?>" data-description="<?php echo htmlspecialchars($product->description); ?>" data-image="<?php echo FILEPATH; ?>/assets/img/product/<?php echo $product->image; ?>" data-id="<?php echo htmlspecialchars($product->id); ?>">
+                <h5><?php echo htmlspecialchars($product->product_name); ?></h5>
+                <div class="price">₱<?php echo $product->price; ?></div>
+                <input type="hidden" value="<?php echo $product->id ?>">
+                <input type="hidden" value="<?php echo $customer_id; ?>">
+                <input type="hidden" value="<?php echo $quantity; ?>">
+                <button type="submit" name="add-to-cart" class="addCart"> Add To Cart</button>
+            </div>
+
+        <?php endforeach; ?>
+
+        <!-- Close the listProduct div after the last category -->
+        <?php if ($currentCategory !== null) : ?>
+            </div>
+        <?php endif; ?>
 
     </div>
+   
     <!--Checkout Session-->
     <div class="cartTab">
 
@@ -309,7 +319,7 @@ if (empty($_SESSION['email'])) {
             <p>Please submit this order ID to the counter section. Thank you!</p>
         </div>
 
-        <button class="btn" id="understand">I understand</button>
+        <button class="btn" id="understand" onClick="window.location.href=window.location.href">I understand</button>
 
     </div>
 
@@ -349,12 +359,15 @@ if (empty($_SESSION['email'])) {
         $(".addCart").on("click", function () {
             // Get product information from the clicked item
             var product = {
-                id: $(this).siblings("input[type=hidden]").val(),
-                name: $(this).siblings("h5").text(),
-                price: $(this).siblings(".price").text(),
-                image: $(this).siblings("img").attr("src"),
-                quantity: 1 // You can set an initial quantity here
-            };
+            id: $(this).siblings("input[type=hidden]").val(),
+            name: $(this).siblings("h5").text(),
+            price: $(this).siblings(".price").text(),
+            image: $(this).siblings("img").attr("src"),
+            category: $(this).siblings("img").data("category"), // Correct the attribute to fetch category
+            quantity: 1 // You can set an initial quantity here
+        };
+
+
 
             // Check if the product is already in the cart
             var existingProductIndex = cart.findIndex(function (item) {
@@ -381,22 +394,23 @@ if (empty($_SESSION['email'])) {
             // Display each item in the cart
             cart.forEach(function (item, index) {
                 var cartItemHtml = '<div class="item">' +
-                    '<img src="' + item.image + '" alt="">' +
-                    '<div class="name">' +
-                    '<div class="name">' + item.name + '</div>' +
-                    '<div class="variant">VARIANT</div>' +
-                    '</div>' +
-                    '<div class="totalPrice">' + item.price + '</div>' +
-                    '<div class="quantity">' +
-                    '<span class="minus" data-index="' + index + '">&lt;</span>' +
-                    '<span>' + item.quantity + '</span>' +
-                    '<span class="plus" data-index="' + index + '">&gt;</span>' +
-                    '</div>' +
-                    '<i class="bx bx-trash icon delete-btn" data-index="' + index + '"></i>' +
-                    '</div>';
+                '<img src="' + item.image + '" alt="">' +
+                '<div class="name">' +
+                '<div class="name">' + item.name + '</div>' +
+                '<div class="category">Category: ' + item.category + '</div>' + // Correct the attribute to fetch category
+                '</div>' +
+                '<div class="totalPrice">' + item.price + '</div>' +
+                '<div class="quantity">' +
+                '<span class="minus" data-index="' + index + '">&lt;</span>' +
+                '<span>' + item.quantity + '</span>' +
+                '<span class="plus" data-index="' + index + '">&gt;</span>' +
+                '</div>' +
+                '<i class="bx bx-trash icon delete-btn" data-index="' + index + '"></i>' +
+                '</div>';
 
-                $(".listCart").append(cartItemHtml);
-            });
+
+            $(".listCart").append(cartItemHtml);
+        });
 
             // Update the cart icon with the total number of items in the cart
             $(".icon-cart span").text(cart.length);
