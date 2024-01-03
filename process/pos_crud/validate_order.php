f7e8e0aadc6783dc27b3e73321501ea987e14aa5
ShanShenShan <shankhylep@gmail.com>
2024-01-03 23:39:44 +0800

    storage_quantity fix

--- process/pos_crud/validate_order.php
+++ process/pos_crud/validate_order.php
@@ -15,8 +15,9 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
     // Query to insert entries into pending_orders
     $insertQuery = $connection->prepare("INSERT INTO pending_orders (o_id, product_id, customer_id, o_quantity, storage_quantity)
-                                        SELECT po.o_id, po.product_id, po.customer_id, po.o_quantity, po.storage_quantity
+                                        SELECT po.o_id, po.product_id, po.customer_id, po.o_quantity, (inv.quantity - po.o_quantity) AS storageQuantityValue
                                         FROM pending_order_kiosk po
+                                        JOIN inventory inv ON po.product_id = inv.id
                                         WHERE po.o_id = :orderId");
     $insertQuery->bindParam(':orderId', $orderId, PDO::PARAM_INT);
     $insertQuery->execute();
